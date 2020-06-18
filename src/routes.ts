0793d7aa75df0c7702c9041680bd090acc5d7116
lucasew <lucas59356@gmail.com>
2020-06-18 17:38:30 +0000

    Add routes
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ src/routes.ts
@@ -0,0 +1,28 @@
+import Router from 'express-promise-router'
+import puppeteer from 'puppeteer'
+
+import {newExpressEventSource} from './components/eventSources/express'
+import {newTaskPool} from './components/taskPool'
+
+const router = Router()
+const taskPool = newTaskPool(2)
+
+
+// setup express event source
+const [handler, eventSource] = newExpressEventSource(async (data) => data, taskPool)
+eventSource(console.log)
+
+router.get("/teste", handler)
+router.get("/render/:page", async (request, response) => {
+    let browser = await puppeteer.launch({
+        headless: true
+    })
+    let page = await browser.newPage()
+    let frame = await page.goto(request.params.page, {
+        waitUntil: "networkidle2"
+    })
+    response.status(frame?.status() as number).send(frame?.buffer)
+    await Promise.all([page.close(), browser.close()])
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,17 @@
 import express from 'express'
 import morgan from 'morgan'
-import {newExpressEventSource} from './components/eventSources/express'
 import {newRSSEventSource} from './components/eventSources/rss'
-import {newTaskPool} from './components/taskPool'
 import {newTimer} from './components/eventSources/timer'
 import config from './config'
-import puppeteer from 'puppeteer'
-import Router from 'express-promise-router'
+
+import routes from './routes'
 
 let app = express()
 app.use(morgan('tiny'))
 
-let router = Router()
+app.use(routes)
 
-const taskPool = newTaskPool(2)
 const timer = newTimer(10000)
-
-// setup express event source
-const [handler, eventSource] = newExpressEventSource(async (data) => data, taskPool)
-eventSource(console.log)
-router.get("/teste", handler)
-router.get("/render/:page", async (request, response) => {
-    let browser = await puppeteer.launch({
-        headless: true
-    })
-    let page = await browser.newPage()
-    let frame = await page.goto(request.params.page, {
-        waitUntil: "networkidle2"
-    })
-    response.status(frame?.status() as number).send(frame?.buffer)
-    await Promise.all([page.close(), browser.close()])
-})
 
 // try {
 //     let [subs, tick] = newRSSEventSource("https://www.aquiagora.net/rss", taskPool)
@@ -42,5 +23,4 @@ router.get("/render/:page", async (request, response) => {
 
 timer(console.log)
 
-app.use(router)
 app.listen(config.HTTP_PORT)
