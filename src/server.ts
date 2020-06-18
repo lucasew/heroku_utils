06c9651a91fb8965794c4d278a37566971ca025a
lucasew <lucas59356@gmail.com>
2020-06-18 17:27:23 +0000

    Add route render page
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,13 @@ import {newRSSEventSource} from './components/eventSources/rss'
 import {newTaskPool} from './components/taskPool'
 import {newTimer} from './components/eventSources/timer'
 import config from './config'
+import puppeteer from 'puppeteer'
+import Router from 'express-promise-router'
 
 let app = express()
 app.use(morgan('tiny'))
+
+let router = Router()
 
 const taskPool = newTaskPool(2)
 const timer = newTimer(10000)
@@ -15,7 +19,18 @@ const timer = newTimer(10000)
 // setup express event source
 const [handler, eventSource] = newExpressEventSource(async (data) => data, taskPool)
 eventSource(console.log)
-app.get("/teste", handler)
+router.get("/teste", handler)
+router.get("/render/:page", async (request, response) => {
+    let browser = await puppeteer.launch({
+        headless: true
+    })
+    let page = await browser.newPage()
+    let frame = await page.goto(request.params.page, {
+        waitUntil: "networkidle2"
+    })
+    response.status(frame?.status() as number).send(frame?.buffer)
+    await Promise.all([page.close(), browser.close()])
+})
 
 // try {
 //     let [subs, tick] = newRSSEventSource("https://www.aquiagora.net/rss", taskPool)
