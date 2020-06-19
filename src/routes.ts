31282c514118026d588294068928dd0e06eeda31
lucasew <lucas59356@gmail.com>
2020-06-19 00:51:21 +0000

    load
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/routes.ts
+++ src/routes.ts
@@ -38,7 +38,7 @@ router.get("/render/:page", async (request, response) => {
     }, 30000)
     let page = await browser.newPage()
     let frame = await page.goto(request.params.page, {
-        waitUntil: 'networkidle0'
+        waitUntil: 'load'
     })
     await page.waitFor(2000)
     if (frame === undefined || frame === null) {
